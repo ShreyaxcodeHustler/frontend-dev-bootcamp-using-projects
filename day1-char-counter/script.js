e5bfb24eea1d548cdf29969df407b11bf6ab6855
Shreya L <lshreya0505@gmail.com>
2026-07-07 17:26:47 +0530

    Basic text counter project

---
+++ day1-char-counter/script.js
@@ -0,0 +1,10 @@
+const textArea =document.getElementById("myInput");//helps js find the textarea element 
+const counter =document.getElementById("Counter");//helps find the counter element
+//feel free to open the html file in the browser and check whether the html  is able to find the js source and refernce it properly after this step
+//save the file->  Inspect → Console tab) and refresh. Type textarea into the console and press enter run the html file(open with live server)
+
+textArea.addEventListener("input", function(){
+    const length=textArea.value.length;
+    counter.textContent = `${length}/100 characters`;
+    //above code are enough for a basic textcounter project 
+})
